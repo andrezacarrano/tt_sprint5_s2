3568bce49df68fb9b00e485e8bda160bad6c9c8d
dezacarrano <andrezacarrano>
2025-10-16 00:36:35 +0100

    Ajustado uso do streamlit e ajustado gráfico de barras

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
 from utils.pre_process import process_dataframe
+import plotly.express as px
 
 # Lê arquivo de dados
 df_raw = pd.read_csv(r'data/raw/superstore.csv')
@@ -13,8 +14,22 @@ df = process_dataframe(df_raw)
 
 st.title('Superstore Data')
 
+toggle_amostra = st.toggle("Visualiza amostra dos dados?", value=True)
+if toggle_amostra:
+    columns = (st.multiselect(
+        label="Quais colunas você quer visualizar?",
+        options=df.columns.tolist(),
+        default=['order_id', 'sub_category',
+                 'product_name', 'quantity', 'profit']
+    )
+    )
+    st.dataframe(data=df[columns].sample(10, random_state=42))
+
 st.dataframe(
-    data=df
+    data=df[columns]
 )
 
-st.bar_chart(data=df, x='ship_mode', y='sales')
+graf_barra = px.bar(df, x='sub_category', y='sales',
+                    title='Total de Vendas por Modo de Envio', color='ship_mode', barmode='group')
+
+st.plotly_chart(graf_barra)
